8b6694b89d8af69321366c8660c9b4fa444309f8
jxmot <webdev@stupiddog.net>
2022-03-21 11:28:10 -0500

    initial commit, lightbox CSS and JS

---
+++ public_html/assets/js/nobs-lightbox.js
@@ -0,0 +1,47 @@
+/* Lightbox JS
+
+    Originally found at:
+        https://www.w3schools.com/howto/howto_js_lightbox.asp
+
+    Extensively Modified By: 
+        https://github.com/jxmot
+
+    Repository: https://github.com/jxmot/website_template-no_bootstrap
+*/
+
+function lb_openModal() {
+// TODO: emit a signal that will close the menu
+  $('#lb_modal').css('display', 'block');;
+}
+
+function lb_closeModal() {
+  $('#lb_modal').css('display', 'none');;
+}
+
+var lb_slideIndex = 1;
+lb_showSlides(lb_slideIndex);
+
+function lb_plusSlides(n) {
+  lb_showSlides(lb_slideIndex += n);
+}
+
+function lb_currentSlide(n) {
+  lb_showSlides(lb_slideIndex = n);
+}
+
+function lb_showSlides(n) {
+  var i;
+  var slides = $('.lb-mySlides');
+  var dots = $('.lb-demo');
+  if (n > slides.length) {lb_slideIndex = 1}
+  if (n < 1) {lb_slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(' lb-active', '');
+  }
+  slides[lb_slideIndex-1].style.display = 'block';
+  dots[lb_slideIndex-1].className += ' lb-active';
+  $('#lb_caption').html(dots[lb_slideIndex-1].alt);
+}
