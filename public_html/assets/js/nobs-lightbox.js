249f80a01918b22ad62a9071e4af424aa5090538
jxmot <webdev@stupiddog.net>
2022-03-22 12:42:49 -0500

    removed unnecessary call to lb_showSlides()

--- public_html/assets/js/nobs-lightbox.js
+++ public_html/assets/js/nobs-lightbox.js
@@ -19,7 +19,7 @@ function lb_closeModal() {
 }
 
 var lb_slideIndex = 1;
-lb_showSlides(lb_slideIndex);
+// lb_showSlides(lb_slideIndex);
 
 function lb_plusSlides(n) {
 	lb_showSlides(lb_slideIndex += n);
