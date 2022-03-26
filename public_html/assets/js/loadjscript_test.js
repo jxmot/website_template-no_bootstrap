c23b3465ff523cf15dc85198363306310e71a70c
jxmot <webdev@stupiddog.net>
2022-03-26 16:01:03 -0500

    initial commit, loadjscript_test()

---
+++ public_html/assets/js/loadjscript_test.js
@@ -0,0 +1,11 @@
+/*
+    loadjscript_test.js - A function used in testing 
+    util.js:loadJScript()
+
+    Author: https://github.com/jxmot
+    Repository: https://github.com/jxmot/website_template-no_bootstrap
+*/
+function loadjscript_test() {
+    document.getElementById('ljs_test').children[0].innerText = '';
+    document.getElementById('ljs_test').children[0].innerText = 'loadJScript() is WORKING!!!';
+}
