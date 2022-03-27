1e31b0e8e359d7390ccaf4f8396599b29842e896
jxmot <webdev@stupiddog.net>
2022-03-27 15:24:42 -0500

    moved #devdebug code to nobs-devdebug.js:devDebug()

--- public_html/assets/js/nobs.js
+++ public_html/assets/js/nobs.js
@@ -15,47 +15,7 @@ const browserinfo = getBrowser();
 // const badbrowser = browserinfo.mask;
 const badbrowser = (browserMasks[FIREFOX][BMASK] | browserMasks[MOBILE][BMASK]);
 
-// turn on debug stuff? 
-if(isQueryParam('devdebug') === true) {
-    $('#devdebug').show();
-    // load a js file and call a function that's inside
-    loadJScript('./assets/js/loadjscript_test.js', function() {
-        // this function is located in the script above
-        loadjscript_test();
-    });
-    // Wait for a media query state change...
-    var mqstate_devd = mediaqueryState('(min-width: 768px)', function(e) {
-        // Check if the media query is true...
-        if(e.matches) {
-            document.getElementById('mqstate').children[0].innerText = e.media + ' Matched!';
-        } else {
-            document.getElementById('mqstate').children[0].innerText = e.media + ' UNmatched!';
-        }
-    });
-    // Check if the media query is true...
-    if(mqstate_devd.match) {
-        document.getElementById('mqstate').children[0].innerText = mqstate_devd.media + ' Matched!';
-    } else {
-        document.getElementById('mqstate').children[0].innerText = mqstate_devd.media + ' UNmatched!';
-    }
-    // display browser information: browser name, device, and 
-    // a "mask" of bits indicating browser and device.
-    // browserinfo was obtained earlier
-    var binfo = browserinfo;
-    document.getElementById('whobrowse').children[0].innerText = binfo.browser;
-    document.getElementById('device').children[0].innerText = binfo.device;
-    // display the mask value as a zero-padded binary string
-    var mstr = '';
-    if(binfo.mask.toString(2).length < 16) {
-        for(x = 0; x < (16 - binfo.mask.toString(2).length); x++) {
-            mstr = mstr + '0';
-        }
-        mstr = mstr + binfo.mask.toString(2);
-    } else mstr = binfo.mask.toString(2);
-    document.getElementById('mask').children[0].innerText = mstr + ' - 0x' + binfo.mask.toString(16);
-    // is it a BAD browser?
-    document.getElementById('isgood').children[0].innerText = (badbrowser === binfo.mask ? 'BAD' : 'GOOD');
-}
+devDebug();
 
 // running with or without the logo?
 var mklogo = getLogoChoice();
