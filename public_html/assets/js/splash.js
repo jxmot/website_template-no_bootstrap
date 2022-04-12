d1f84174023f4423b4732bc83a384a6dcf175a71
jxmot <webdev@stupiddog.net>
2022-04-12 12:03:08 -0500

    modified to allow two splash animations

--- public_html/assets/js/splash.js
+++ public_html/assets/js/splash.js
@@ -4,9 +4,9 @@
     Author: https://github.com/jxmot
     Repository: https://github.com/jxmot/website_template-no_bootstrap
 */
-// Handle the end of the splash animation
+// Handle the end of the splash animation(s)
 $().ready(() => {
-    document.getElementById('top_splash').addEventListener('animationend', () => {
-        document.getElementById('top_splash').style.display = 'none';
-    },{once:true});
+    document.getElementById('top_splash').addEventListener('animationend', (ev) => {
+        if(ev.animationName === 'splash-opacity') document.getElementById('top_splash').style.display = 'none';
+    });
 });
